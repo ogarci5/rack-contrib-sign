087ff1b18758554040698da260d162094a14e125
Graham Christensen <gchristensen@nationalfield.org>
2013-07-23 16:53:05 -0500

    Refactor, breaking pieces apart.

--- lib/rack/contrib/sign/middleware.rb
+++ lib/rack/contrib/sign/middleware.rb
@@ -12,50 +12,88 @@ module Rack
         end
 
         def call env
-
-          unless env['HTTP_AUTHORIZATION']
-            @logger.info "Denied: Authorization header not present."
+          creds = extract_credentials env['HTTP_AUTHORIZATION']
+          unless creds
+            @logger.info "Denied: Authorization header not present or invalid."
             return [401, {}, []]
           end
 
-          api_key = '123'
-          api_secret = 'abc'
-
-          receipt = Rack::Contrib::Sign::Receipt.new
-          receipt.request_method = env['REQUEST_METHOD']
-          receipt.uri = env['REQUEST_URI']
-          receipt.api_key = api_key
-          receipt.api_secret = api_secret
-          receipt.body = env['rack.input'].read
-
-          env['rack.input'].rewind
-
-          env.sort_by { |k,v| k.to_s.downcase }.each do |key,val|
-            next unless key =~ /^http_#{@header_prefix}/i
-            header = key.sub(/^http_/i, '').gsub(/_/, '-')
-            receipt.headers[header] = val
-          end
+          receipt = build_receipt env
+          receipt.api_key = creds[:key]
+          receipt.api_secret = get_secret creds[:key]
 
           sign = receipt.to_s
 
           digest = OpenSSL::Digest::Digest.new('sha1')
-          validation = OpenSSL::HMAC.hexdigest(digest, api_secret, sign)
+          validation = OpenSSL::HMAC.hexdigest(digest, receipt.api_secret, sign)
 
-          expected_auth_header = "#{@realm} #{api_key}:#{validation}"
-
-          unless env['HTTP_AUTHORIZATION'] == expected_auth_header
+          unless validation == creds[:signature]
             @logger.error "Denied: Authorization signature does not match"
-            @logger.info sprintf(
-              "Denied: EXPECTED: %s GOT: %s",
-              expected_auth_header,
-              env['HTTP_AUTHORIZATION']
-            )
+            @logger.info "Denied: EXPECTED: %s GOT: %s" % [
+              validation,
+              creds[:signature]
+            ]
             @logger.debug "Generated signing data:"
             @logger.debug sign
             return [401, {}, []]
           end
 
           @app.call env
+        end
+
+        # Extract environmental data into a Receipt
+        def build_receipt env
+          receipt = Rack::Contrib::Sign::Receipt.new
+          receipt.uri = env['REQUEST_URI']
+          receipt.request_method = env['REQUEST_METHOD']
+          receipt.body = extract_body env
+
+          extract_headers(env).each { |h,v| receipt.headers[h] = v }
+
+          receipt
+        end
+
+        # Extract the body from the environment, ensuring to rewind
+        # the input back to zero, so future access gets the arguments.
+        def extract_body env
+          env['rack.input'].read
+        ensure
+          env['rack.input'].rewind
+        end
+
+        # Extract all the headers with our Prefix from the ENV
+        # and return the hash
+        def extract_headers env
+          headers = {}
+
+          env.sort_by { |k,v| k.to_s.downcase }.each do |key,val|
+            next unless key =~ /^http_#{@header_prefix}/i
+            header = key.sub(/^http_/i, '').gsub(/_/, '-')
+            headers[header] = val
+          end
+
+          headers
+        end
+
+        # Pass in the Authorization header, and get back the key
+        # and signature.
+        def extract_credentials auth_header
+          return false if auth_header.nil?
+
+          pattern = /^(?<realm>.*) (?<api_key>.*):(?<signature>.*)$/
+          matches = auth_header.match(pattern)
+
+          return false if matches.nil?
+          return false unless matches[:realm] == @realm
+
+          {
+            key: matches[:api_key],
+            signature: matches[:signature]
+          }
+        end
+
+        def get_secret api_key
+          return 'abc'
         end
       end
     end
--- spec/rack/contrib/sign/middleware_spec.rb
+++ spec/rack/contrib/sign/middleware_spec.rb
@@ -23,6 +23,93 @@ describe Rack::Contrib::Sign::Middleware do
     prefix: "HI-"
   )}
 
+  describe "#build_receipt" do
+    it "sets various options" do
+      env = {
+        'REQUEST_METHOD' => 'POST',
+        'REQUEST_URI' => 'foo/bar',
+        'HTTP_AUTHORIZATION' => 'foo-bar 123:mysignature',
+        'HTTP_HI_FOOO' => 'YIPEE',
+        'rack.input' => StringIO.new('foo=bar')
+      }
+
+      receipt = ware.build_receipt env
+
+      receipt.uri.should eq('foo/bar')
+      receipt.request_method.should eq('POST')
+      receipt.body.should eq('foo=bar')
+      receipt.headers.should eq({
+        'HI-FOOO' => 'YIPEE',
+      })
+
+    end
+  end
+
+  describe "#extract_body" do
+    it "returns the body of a request from the environment" do
+      env = {
+        'rack.input' => StringIO.new('fooz=baz')
+      }
+
+      ware.extract_body(env).should eq('fooz=baz')
+    end
+
+    it "rewinds the input after reading" do
+      str = StringIO.new('fooz=baz')
+      env = {
+        'rack.input' => str
+      }
+
+      str.pos.should eq(0)
+    end
+  end
+
+
+  describe "#extract_headers" do
+    it "returns headers prefixed with HI-" do
+      headers = {
+        'HTTP_HI_FOO' => ':/',
+        'HTTP_BYE_FOO' => ':(',
+        'HTTP_HI_oentuheou' => ':)'
+      }
+
+      found_headers = ware.extract_headers(headers)
+
+      expected_headers = {
+        'HI-FOO' => ':/',
+        'HI-oentuheou' => ':)'
+      }
+      found_headers.should eq(expected_headers)
+    end
+  end
+
+  describe "#extract_credentials" do
+    it "returns false when nil is provided" do
+      header = nil
+
+      ware.extract_credentials(header).should eq(false)
+    end
+
+    it "returns false when the header is garbled" do
+      header = "LOL WHAT I DON'T EVEN"
+
+      ware.extract_credentials(header).should eq(false)
+    end
+
+    it "returns my key and signature when a formatted string is provided" do
+      header = 'foo-bar mykey:signature'
+
+      ware.extract_credentials(header).should eq(key: 'mykey', signature: 'signature')
+    end
+
+    it "returns false if the realm fails to match" do
+      header = 'foo-bar-baz mykey:signature'
+
+      ware.extract_credentials(header).should eq(false)
+    end
+
+  end
+
   describe "#call" do
     context "I have no other tests" do
       it "abandons ship if there is no authorization header" do
@@ -31,12 +118,12 @@ describe Rack::Contrib::Sign::Middleware do
         returned = ware.call(env)
 
         returned.should eq([401, {}, []])
-        log_string.string.should eq("INFO - Denied: Authorization header not present.\n")
+        log_string.string.should eq("INFO - Denied: Authorization header not present or invalid.\n")
       end
 
       it "401s when I don't sign it right" do
         env = {
-          'HTTP_AUTHORIZATION' => 'foo-bar YABBA DABBA DOOO',
+          'HTTP_AUTHORIZATION' => 'foo-bar abc:YABBA DABBA DOOO',
           'REQUEST_METHOD' => 'POST',
           'REQUEST_URI' => 'http://foo/bar/baz',
           'rack.input' => StringIO.new('foo=bar'),
